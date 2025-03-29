1574e41140bd48ffa99a1026322616542b382a73
leojp04 <leojp04@gmail.com>
2025-03-29 00:40:26 -0300

    Aula Tenis Final

--- src/Principal.java
+++ src/Principal.java
@@ -5,21 +5,36 @@ public class Principal {
     public static void main(String[] args) {
     //se digitar main ele já monta toda essa frase acima
         // criar um objeto tenis com os valores digitados.
-        //Exibir os valoes que estão dentro do objeto
+        //Exibir os valores que estão dentro do objeto
         // Criar o objeto responsavel por ler os dados digitados no teclado
 
-        Scanner tenis = new Scanner(System.in);
+        Scanner leitor = new Scanner(System.in);
 
         System.out.println("Digite a cor do ténis que procura");
-        String cor = tenis.next() + tenis.nextLine(); //Le a String com mais de uma palavra
+        String cor = leitor.next() + leitor.nextLine(); //Le a String com mais de uma palavra
         System.out.println("Digite o modelo do Tenis");
-        String modelo = tenis.next() + tenis.nextLine();
+        String modelo = leitor.next() + leitor.nextLine();
         System.out.println("Digite o tamanho do tênis");
-        double tamanho = tenis.nextDouble();
+        double tamanho = leitor.nextDouble();
         System.out.println( "Digite o preço que está procurando");
-        double preco = tenis.nextDouble();
+        double preco = leitor.nextDouble();
         System.out.println("Gostaria de procurar algo a pronta entrega? True or False");
-        boolean disponivel = tenis.nextBoolean();// quando rodar o programa escreve true or false
+        boolean disponivel = leitor.nextBoolean();// quando rodar o programa escreve true or false
+
+        //Ler as informações da marca
+        System.out.println("Digite o nome da marca");
+        String nomeMarca = leitor.next() + leitor.nextLine();
+
+        System.out.println("Digite o nome do arquivo da logo");
+        String logo = leitor.next() + leitor.nextLine();
+
+        //Criar o objeto marca 
+        Marca marca= new Marca();
+
+        //Atribuir os valores ao objeto
+        marca.nome = nomeMarca;
+        marca.logo = logo;
+   
 
         //Criar o objeto Tenis
         Tenis tenis1 = new Tenis();
@@ -29,6 +44,7 @@ public class Principal {
         tenis1.disponivel = disponivel;
         tenis1.tamanho = tamanho;
         tenis1.preco = preco;
+        tenis1.marca = marca;
 
         //Exibir os valores que estão dentro do objeto
         System.out.println("Modelo:" + tenis1.modelo);
@@ -36,5 +52,16 @@ public class Principal {
         System.out.println("Disponivel" + tenis1.disponivel);
         System.out.println("Tamanho" + tenis1.tamanho);
         System.out.println("Preço" + tenis1.preco);
+        //Exibir os dados da marca (através do objeto tenis)
+        System.out.println("Marca" + tenis1.marca.nome);
+        System.out.println("Logo" +tenis1.marca.logo);
+
+        //Chamar (invoar) o método para aumentar o valor em 15
+        double aumento = tenis1.checarPreco(15);
+        tenis1.aumentarPreco(aumento); // Só passar o valor do float. O nome porcentagem já está atribuido
+
+        //Exibir o valor atualizado do tenis
+        System.out.println("Valor atualizado: "+ tenis1.preco);
+        System.out.println("O aumento foi de: "+ aumento);
     }
 }
--- src/Tenis.java
+++ src/Tenis.java
@@ -1,9 +1,26 @@
 public class Tenis {
 
+
     String cor;
     String modelo;
     double tamanho;
     double preco;
     boolean disponivel;
+
+    //Atributo de referencia(outra classe que iremos criar)
+    Marca marca;
+
+    //Métodos
+    //Método aumentar o preço do tenis em porcentagem (10,20)
+
+        double checarPreco(float porcentagem){
+            return preco * porcentagem/100;
+
+    }
+        void aumentarPreco(double valor){
+            preco += valor;
+            //Quando você vai criar um método que faz algo, mas não precisa devolver um valor (como exibir uma mensagem, realizar um cálculo, etc.), você usa o void
+            //É como uma "instrução" que você pode chamar para fazer algo específico, como calcular algo, exibir informações, alterar valores, etc.
+    }
 }
 
